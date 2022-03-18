3022b6bc538a6b38de93ab74ed35fa685ff6f9ee
robinjoon <0108dlatnqls@gmail.com>
2022-03-18 22:12:55 +0900

    MemberRepository 메서드 이름 변경

--- src/main/java/com/j2kb/goal/repository/MemberRepository.java
+++ src/main/java/com/j2kb/goal/repository/MemberRepository.java
@@ -4,7 +4,7 @@ import com.j2kb.goal.dto.Member;
 
 public interface MemberRepository {
     void insertMember(Member member);
-    Member selectMemberByMemberId(String MemberId);
+    Member selectMemberByMemberEmail(String memberEmail);
     void updateMember(Member member);
     void deleteMember(Member member);
 }
