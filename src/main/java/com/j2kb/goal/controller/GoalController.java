04ee6ae210cfbf7f34a85251d798f1ce801f158d
robinjoon <0108dlatnqls@gmail.com>
2022-03-29 22:03:50 +0900

    api 추가

--- src/main/java/com/j2kb/goal/controller/GoalController.java
+++ src/main/java/com/j2kb/goal/controller/GoalController.java
@@ -1,13 +1,16 @@
 package com.j2kb.goal.controller;
 
+import com.j2kb.goal.dto.Goal;
 import com.j2kb.goal.repository.GoalRepository;
 import com.j2kb.goal.repository.JdbcTemplateGoalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/goals")
@@ -24,4 +27,15 @@ public class GoalController {
     public List<String> getCategories(){
         return repository.selectAllCategories();
     }
+
+    @GetMapping("/{goalId:[0-9]+}")
+    public Goal getGoalByGoalId(@PathVariable long goalId){
+        return repository.selectGoalByGoalId(goalId).orElse(Goal.builder().build());
+    }
+
+    @GetMapping("/{category}/list")
+    public List<Goal> getGoalsByCategory(@PathVariable String category){
+        return repository.selectAllGoalsByCategory(category);
+    }
+
 }
