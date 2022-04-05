ef0af7397be03f139a1918f121bc3b81593571c8
robinjoon <0108dlatnqls@gmail.com>
2022-04-05 21:14:35 +0900

    검증 인터셉터 추가

---
+++ src/main/java/com/j2kb/goal/intercepter/MemberCertInterceptor.java
@@ -0,0 +1,33 @@
+package com.j2kb.goal.intercepter;
+
+
+import com.j2kb.goal.repository.MemberRepository;
+import com.j2kb.goal.util.JwtBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@Component
+public class MemberCertInterceptor implements HandlerInterceptor {
+    @Autowired
+    private MemberRepository memberRepository;
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        String auth = request.getHeader("Authorization");
+        if(JwtBuilder.isValid(auth)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                            @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+}
