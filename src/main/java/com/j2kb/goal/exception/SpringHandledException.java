92c1211ac0b4a7445d51661f7e5bba7f274588f4
robinjoon <0108dlatnqls@gmail.com>
2022-06-10 22:47:58 +0900

    Error에 에러 메시지,타임스탬프 추가

--- src/main/java/com/j2kb/goal/exception/SpringHandledException.java
+++ src/main/java/com/j2kb/goal/exception/SpringHandledException.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 public class SpringHandledException extends RuntimeException{
     private HttpStatus httpStatus;
     private int errorCode;
@@ -16,14 +19,17 @@ public class SpringHandledException extends RuntimeException{
         this.httpStatus = httpStatus;
         this.errorCode = errorCode;
         this.url = url;
+        this.msg = msg;
     }
     @AllArgsConstructor
     @Getter
     private class Error{
         private int errorCode;
+        private String errorContent;
         private String url;
+        private Timestamp timestamp;
     }
     public final ResponseEntity<Error> parseResponseEntity(){
-        return ResponseEntity.status(httpStatus).body(new Error(errorCode,url));
+        return ResponseEntity.status(httpStatus).body(new Error(errorCode,msg,url,Timestamp.valueOf(LocalDateTime.now())));
     }
 }
